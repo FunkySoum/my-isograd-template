be778ed9c1f370d87607125fad7ab7109ac1c823
FunkySoum <99559582+FunkySoum@users.noreply.github.com>
2023-02-20 20:12:46 +0100

    Initialisation

---
+++ src/main/java/com/isograd/exercise/IsoContest.java
@@ -0,0 +1,36 @@
+/*******
+ * Read input from System.in
+ * Use System.out.println to ouput your result.
+ * Use:
+ *  IsoContestBase.localEcho( variable)
+ * to display variable in a dedicated area.
+ * ***/
+package com.isograd.exercise;
+
+import java.util.*;
+
+
+public class IsoContest {
+	
+	private static Vector<String> lines = null;
+	
+	public static void main(String[] argv) throws Exception {
+		
+		IsoContest isoContest = new IsoContest();
+		
+		String line;
+		Scanner sc = new Scanner(System.in);
+		lines = new Vector<>();
+		while (sc.hasNextLine()) {
+			line = sc.nextLine();
+			lines.add(line);
+		}
+		
+		
+		
+	}
+	
+	
+}
+
+
