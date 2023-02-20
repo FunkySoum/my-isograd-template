43c712a059dabf8634f29790f6f19eb6cd866d83
FunkySoum <99559582+FunkySoum@users.noreply.github.com>
2023-02-20 20:27:03 +0100

    Ajout methodes de lecture avec cast

--- src/main/java/com/isograd/exercise/IsoContest.java
+++ src/main/java/com/isograd/exercise/IsoContest.java
@@ -31,6 +31,48 @@ public class IsoContest {
 	}
 	
 	
+	
+	public static String getStringInLines(int numeroLigne) {
+		return lines.get(numeroLigne);
+	}
+	
+	public static String getStringInLines(int numeroLigne, int position) {
+		return getStringInLines(numeroLigne, position, " ");
+	}
+	
+	private static String getStringInLines(int numeroLigne, int position, String separateur) {
+		String[] data = lines.get(numeroLigne).split(separateur);
+		return data[position];
+	}
+	
+	public static int getIntInLines(int numeroLigne) {
+		return Integer.parseInt(lines.get(numeroLigne));
+	}
+	
+	public static int getIntInLines(int numeroLigne, int position) {
+		return getIntInLines(numeroLigne, position, " ");
+	}
+	
+	private static int getIntInLines(int numeroLigne, int position, String separateur) {
+		String[] data = lines.get(numeroLigne).split(separateur);
+		return Integer.parseInt(data[position]);
+	}
+	
+	public static int getHeureInLines(int numeroLigne) {
+		String[] heure = lines.get(numeroLigne).split(":");
+		return Integer.parseInt(heure[0])*60+Integer.parseInt(heure[1]);
+	}
+	
+	public static int getHeureInLines(int numeroLigne, int position) {
+		return getHeureInLines(numeroLigne, position, " ");
+	}
+	
+	private static int getHeureInLines(int numeroLigne, int position, String separateur) {
+		String[] data = lines.get(numeroLigne).split(separateur);
+		String[] heure = data[position].split(":");
+		return Integer.parseInt(heure[0])*60+Integer.parseInt(heure[1]);
+	}
+	
 }
 
 
